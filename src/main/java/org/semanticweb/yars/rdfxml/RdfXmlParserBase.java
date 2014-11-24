888f33a795f18339d268bc89bea7d665d122c6b5
tob.kaefer@gmail.com <tob.kaefer@gmail.com@72b415bf-7cfd-ce19-ccc8-9e7926a2b84a>
2014-11-24 04:43:06 +0000

    avoiding unnecessary unescaping

--- src/main/java/org/semanticweb/yars/rdfxml/RdfXmlParserBase.java
+++ src/main/java/org/semanticweb/yars/rdfxml/RdfXmlParserBase.java
@@ -950,7 +950,7 @@ public class RdfXmlParserBase extends DefaultHandler {
 	}
 
 	private static boolean isCMP(Resource p1) {
-		return p1.equals(RDF.LI) || p1.getUriString().startsWith(RDF.NS+"_");
+		return p1.equals(RDF.LI) || p1.toString().startsWith("<" + RDF.NS + "_");
 	}
 
 	private void handleParseTypeLiteralEndElement(final String name, final String lname, final String qname){
