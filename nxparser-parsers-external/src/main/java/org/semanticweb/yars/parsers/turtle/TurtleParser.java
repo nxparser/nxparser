203a8d1be7a1c01533574ee2b412021f97accf47
tob.kaefer@gmail.com <tob.kaefer@gmail.com@72b415bf-7cfd-ce19-ccc8-9e7926a2b84a>
2015-03-23 16:11:04 +0000

    External Turtle Parser

---
+++ nxparser-parsers-external/src/main/java/org/semanticweb/yars/parsers/turtle/TurtleParser.java
@@ -0,0 +1,100 @@
+package org.semanticweb.yars.parsers.turtle;
+
+import java.io.InputStream;
+import java.util.Iterator;
+import java.util.logging.Logger;
+
+import org.apache.jena.riot.RDFLanguages;
+import org.apache.jena.riot.RiotException;
+import org.apache.jena.riot.RiotParseException;
+import org.apache.jena.riot.lang.LangTurtle;
+import org.apache.jena.riot.lang.PipedRDFIterator;
+import org.apache.jena.riot.lang.PipedTriplesStream;
+import org.apache.jena.riot.system.RiotLib;
+import org.apache.jena.riot.tokens.TokenizerFactory;
+import org.semanticweb.yars.nx.BNode;
+import org.semanticweb.yars.nx.Literal;
+import org.semanticweb.yars.nx.Node;
+import org.semanticweb.yars.nx.Resource;
+
+import com.hp.hpl.jena.graph.Node_Blank;
+import com.hp.hpl.jena.graph.Node_Literal;
+import com.hp.hpl.jena.graph.Node_URI;
+import com.hp.hpl.jena.graph.Triple;
+
+public class TurtleParser implements Iterator<Node[]>, Iterable<Node[]> {
+
+	private static Logger _log = Logger.getLogger(TurtleParser.class.getName());
+
+	PipedRDFIterator<Triple> _tripleIterator;
+	String _baseURI;
+	Resource _resBaseURI;
+
+	public TurtleParser() {
+
+	}
+
+	public void parse(InputStream is, String baseURI) {
+		_tripleIterator = new PipedRDFIterator<Triple>();
+		_baseURI = baseURI;
+		_resBaseURI = new Resource(_baseURI, false);
+
+		LangTurtle lt = new LangTurtle(TokenizerFactory.makeTokenizerUTF8(is),
+				RiotLib.profile(RDFLanguages.TURTLE, baseURI),
+				new PipedTriplesStream(_tripleIterator));
+		try {
+			lt.parse();
+		} catch (RiotParseException e1) {
+			_log.warning("Moving on to the next line, as I couldn't parse line "
+					+ e1.getLine()
+					+ ". Error at col "
+					+ e1.getCol()
+					+ ": "
+					+ e1.getMessage());
+		} catch (RiotException e) {
+			_log.warning("Moving on to the next line, as I couldn't parse line whatever. Message: "
+					+ e.getMessage());
+		}
+	}
+
+	@Override
+	public Iterator<Node[]> iterator() {
+		return this;
+	}
+
+	@Override
+	public boolean hasNext() {
+		return _tripleIterator.hasNext();
+	}
+
+	@Override
+	public Node[] next() {
+		Triple q = _tripleIterator.next();
+		return new Node[] { jenaToNxParser(q.getSubject()),
+				jenaToNxParser(q.getPredicate()),
+				jenaToNxParser(q.getObject()), _resBaseURI };
+
+	}
+
+	public org.semanticweb.yars.nx.Node jenaToNxParser(
+			com.hp.hpl.jena.graph.Node jenaNode) {
+		if (jenaNode instanceof Node_URI)
+			return new Resource(jenaNode.getURI(), false);
+		else if (jenaNode instanceof Node_Blank)
+			return BNode.createBNode(_baseURI, jenaNode.getBlankNodeLabel());
+		else if (jenaNode instanceof Node_Literal) {
+			return new Literal(jenaNode.getLiteralLexicalForm(),
+					jenaNode.getLiteralLanguage() == null ? null : jenaNode
+							.getLiteralLanguage().equals("") ? null : jenaNode
+							.getLiteralLanguage(),
+					jenaNode.getLiteralDatatypeURI() == null ? null : jenaNode
+							.getLiteralDatatypeURI().equals("") ? null
+							: new Resource(jenaNode.getLiteralDatatypeURI(),
+									false));
+		} else
+			// shouldn't happen
+			return null;
+
+	}
+
+}
---
+++ nxparser-parsers-external/src/test/java/org/semanticweb/yars/parsers/turtle/TurtleParserTest.java
@@ -0,0 +1,67 @@
+package org.semanticweb.yars.parsers.turtle;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringWriter;
+
+import org.junit.Test;
+import org.semanticweb.yars.nx.Node;
+import org.semanticweb.yars.nx.Nodes;
+
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+
+public class TurtleParserTest {
+
+	@Test
+	public void test() throws IOException {
+		String turtleString = "@prefix ex: <http://example.org/> .\n"
+				+ "<> a ex:document , [ a ex:bla ] ; ex:lsdf ex:123.\n"
+				+ "</213> ex:hasMembers (ex:SnoopDogg ex:NateDogg) .\n"
+				+ "<> ex:label \"lskdfsdf\" \n"
+				+ ", \"lskdfsdf\"@de-de  \n"
+				+ ", \"lskdäääääfsdf\"^^ex:datatype , \n \"\"\"dsfsdf\nsdfsdfsdf\ndfsd sdfsdf\nsdfsdf\"\"\""
+				+ " .\n";
+
+		String baseURI = "http://base.uri/";
+
+		InputStream is = new ByteArrayInputStream(turtleString.getBytes());
+
+		TurtleParser tp = new TurtleParser();
+		tp.parse(is, baseURI);
+
+		Model nxparserModel = createModelFromNodesCollection(tp);
+
+		Model turtleModel = ModelFactory.createDefaultModel();
+
+		is = new ByteArrayInputStream(turtleString.getBytes());
+		turtleModel.read(is, baseURI, "TURTLE");
+
+		assertTrue(nxparserModel.isIsomorphicWith(turtleModel));
+
+	}
+
+	public static Model createModelFromNodesCollection(Iterable<Node[]> cns)
+			throws IOException {
+		StringWriter sw = new StringWriter();
+
+		Model modelTest = ModelFactory.createDefaultModel();
+
+		for (Node[] ns : cns) {
+			sw.write(Nodes.toString(new Node[] { ns[0], ns[1], ns[2] }));
+			sw.write('\n');
+		}
+		sw.close();
+
+		InputStream is2 = new ByteArrayInputStream(sw.getBuffer().toString()
+				.getBytes());
+		modelTest.read(is2, "", "N-TRIPLES");
+		is2.close();
+
+		return modelTest;
+
+	}
+}
