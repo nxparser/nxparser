fc7f3f5c8e7a4e32a2948fbcbbb361b241f58669
Tobias Käfer <tobias_kaefer@yahoo.de>
2015-05-21 22:56:56 +0200

    Removed unused import

--- nxparser-model-datatypes/src/main/java/org/semanticweb/yars/nx/dt/datetime/Iso8601Parser.java
+++ nxparser-model-datatypes/src/main/java/org/semanticweb/yars/nx/dt/datetime/Iso8601Parser.java
@@ -6,7 +6,6 @@ import java.util.StringTokenizer;
 import java.util.TimeZone;
 
 import org.semanticweb.yars.nx.dt.DatatypeParseException;
-import org.semanticweb.yars.nx.parser.ParseException;
 
 public class Iso8601Parser {	
 	private static String NO_TOKEN = "\n";
