1f8b8c245109da8a64366331211eff9d6527e070
Tobias Käfer <tobias_kaefer@yahoo.de>
2015-07-28 11:57:30 +0200

    playing around with testcases

--- nxparser-parsers-external/src/test/java/org/semanticweb/yars/parsers/jsonld/JsonLDParserTest.java
+++ nxparser-parsers-external/src/test/java/org/semanticweb/yars/parsers/jsonld/JsonLDParserTest.java
@@ -20,12 +20,15 @@ public class JsonLDParserTest {
 	@Test
 	public void test() throws ParseException {
 
-		String s = "{" + " \"@context\": \"http://schema.org/\", "
+		String s = "{" 
+				+ " \"@context\": \"http://schema.org/\", " 
+				+ "\"@id\":\"sdf\","
 				+ " \"@type\": \"Person\", " 
 				+ " \"name\": \"Jane Doe\", "
 				+ " \"jobTitle\": \"Professor\", "
 				+ " \"telephone\": \"(425) 123-4567\", "
-				+ " \"url\": \"http://www.janedoe.com\"" + "}";
+				+ " \"url\": \"http://www.janedoe.com\"" 
+				+ "}";
 		
 		System.err.println("Testing using:");
 		System.err.println(s);
@@ -45,12 +48,15 @@ public class JsonLDParserTest {
 	@Test
 	public void testSemargl() throws ParseException {
 		
-		String s = "{" + " \"@context\": \"http://schema.org/\", "
+		String s = "{" 
+				+ " \"@context\": \"http://schema.org/\", " 
+				//+ "\"@id\":\"sdf\","
 				+ " \"@type\": \"Person\", " 
 				+ " \"name\": \"Jane Doe\", "
 				+ " \"jobTitle\": \"Professor\", "
 				+ " \"telephone\": \"(425) 123-4567\", "
-				+ " \"url\": \"http://www.janedoe.com\"" + "}";
+				+ " \"url\": \"http://www.janedoe.com\"" 
+				+ "}";
 		
 		CharOutputSink cos = new CharOutputSink();
 		cos.connect(System.err);
@@ -61,4 +67,40 @@ public class JsonLDParserTest {
 		
 	}
 
+	@Test
+	public void test2() throws ParseException {
+		// official test case http://json-ld.org/test-suite/tests/toRdf-0042-in.jsonld
+		
+
+		String s = "{  "
+				+ "\"@context\": "
+				//+ "{   "
+				//+ "\"@base\":"
+				+ "\"http://ex.org/\", "
+				//+ "\"t1\": \"http://example.com/t1\",  "
+				//+ "\"t2\": \"http://example.com/t2\",    "
+				//+ "\"term1\": \"http://example.com/term1\",    "
+				//+ "\"term2\": \"http://example.com/term2\",    "
+				//+ "\"term3\": \"http://example.com/term3\",    "
+				//+ "\"term4\": \"http://example.com/term4\",    "
+				//+ "\"term5\": \"http://example.com/term5\"  "
+				//+ "}"
+				//+ ",  "
+				+ "\"@id\": \"http://example.com/id1\",  \"@type\": \"t1\",  \"term1\": \"v1\",  \"term2\": {\"@value\": \"v2\", \"@type\": \"t2\"},  \"term3\": {\"@value\": \"v3\", \"@language\": \"en\"},  \"term4\": 4,  \"term5\": [50, 51]}";
+		
+		System.err.println("Testing using:");
+		System.err.println(s);
+		
+		System.err.println();
+		System.err.println("Result:");
+		
+		JsonLDParser jlp = new JsonLDParser();
+		jlp.parse(new ByteArrayInputStream(s.getBytes()), "http://example.org/");
+		
+		for (Node[] nx:jlp) {
+			System.err.println(Nodes.toString(nx));
+		}
+		
+	}
+	
 }
