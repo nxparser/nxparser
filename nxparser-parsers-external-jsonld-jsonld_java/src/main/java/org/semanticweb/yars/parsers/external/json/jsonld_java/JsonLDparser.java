84a6afd357c7af1be07aeda2e6aa2f9f123b7802
Tobias Käfer <tobias.kaefer@kit.edu>
2016-01-22 19:59:34 +0100

    JSON-LD parser with new interface

--- nxparser-parsers-external-jsonld-jsonld_java/src/main/java/org/semanticweb/yars/parsers/external/json/jsonld_java/JsonLDparser.java
+++ nxparser-parsers-external-jsonld-jsonld_java/src/main/java/org/semanticweb/yars/parsers/external/json/jsonld_java/JsonLDparser.java
@@ -2,46 +2,155 @@ package org.semanticweb.yars.parsers.external.json.jsonld_java;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Iterator;
+import java.io.Reader;
+import java.net.URI;
+import java.nio.charset.Charset;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+import org.semanticweb.yars.nx.BNode;
+import org.semanticweb.yars.nx.Literal;
 import org.semanticweb.yars.nx.Node;
+import org.semanticweb.yars.nx.Resource;
+import org.semanticweb.yars.nx.parser.Callback;
+import org.semanticweb.yars.nx.parser.ErrorHandler;
+import org.semanticweb.yars.nx.parser.RdfParser;
 
 import com.github.jsonldjava.core.DocumentLoader;
 import com.github.jsonldjava.core.JsonLdError;
 import com.github.jsonldjava.core.JsonLdOptions;
 import com.github.jsonldjava.core.JsonLdProcessor;
+import com.github.jsonldjava.core.JsonLdTripleCallback;
+import com.github.jsonldjava.core.RDFDataset;
+import com.github.jsonldjava.core.RDFDataset.Quad;
 import com.github.jsonldjava.utils.JsonUtils;
+
 /**
  * Parses JSON-LD.
  *
  * @author Tobias Käfer
  *
  */
-public class JsonLDparser implements Iterable<Node[]> {
+public class JsonLDparser implements RdfParser {
 
-	JsonLDtripleCallback _callback;
+	private static final Logger LOG = Logger.getLogger(JsonLDparser.class.getName());
+
+	private static enum Contents {
+		reader, inputstreamAndCharset, inputstream
+	};
+
+	final Object[] _inputData;
+	final Contents _inputDataContents;
+
+	private ErrorHandler _eh;
 
 	static final DocumentLoader _dl = new DocumentLoader();
 
-	public void parse(final InputStream in, final String sourceURI)
-			throws JsonLdError, IOException {
-		_callback = new JsonLDtripleCallback(sourceURI);
+	private final JsonLdOptions _options;
 
-		final JsonLdOptions options = new JsonLdOptions(sourceURI);
+	private final String _baseURIstring;
 
-		options.setDocumentLoader(_dl);
-
-		JsonLdProcessor
-				.toRDF(JsonUtils.fromInputStream(in), _callback, options);
+	public JsonLDparser(InputStream is, URI baseURI) {
+		_options = new JsonLdOptions(baseURI.toString());
+		_options.setDocumentLoader(_dl);
+		_baseURIstring = baseURI.toString();
+		_inputData = new Object[] { is };
+		_inputDataContents = Contents.inputstream;
 	}
 
-	@Override
-	public Iterator<Node[]> iterator() {
-		return _callback.getNx().iterator();
+	public JsonLDparser(InputStream is, Charset cs, URI baseURI) {
+		_options = new JsonLdOptions(baseURI.toString());
+		_options.setDocumentLoader(_dl);
+		_baseURIstring = baseURI.toString();
+		_inputData = new Object[] { is, cs };
+		_inputDataContents = Contents.inputstreamAndCharset;
+	}
+
+	public JsonLDparser(Reader r, URI baseURI) {
+		_options = new JsonLdOptions(baseURI.toString());
+		_options.setDocumentLoader(_dl);
+		_baseURIstring = baseURI.toString();
+		_inputData = new Object[] { r };
+		_inputDataContents = Contents.reader;
+	}
+
+	public void parse(final Callback cb) {
+
+		cb.startDocument();
+
+		final Object input;
+
+		try {
+			switch (_inputDataContents) {
+			case reader:
+				input = JsonUtils.fromReader((Reader) _inputData[0]);
+				break;
+			default:
+				// default should never happen
+			case inputstream:
+				input = JsonUtils.fromInputStream((InputStream) _inputData[0]);
+				break;
+			case inputstreamAndCharset:
+				input = JsonUtils.fromInputStream((InputStream) _inputData[0], ((Charset) _inputData[1]).name());
+				break;
+			}
+
+			JsonLdProcessor.toRDF(input, new JsonLdTripleCallback() {
+
+				@Override
+				public Object call(RDFDataset dataset) {
+					for (String graphName : dataset.graphNames()) {
+						for (Quad quad : dataset.getQuads(graphName)) {
+							cb.processStatement(
+									new Node[] { jsonld_java_Node__to__nxparser_Node(quad.getSubject(), _baseURIstring),
+											jsonld_java_Node__to__nxparser_Node(quad.getPredicate(), _baseURIstring),
+											jsonld_java_Node__to__nxparser_Node(quad.getObject(), _baseURIstring) });
+						}
+					}
+					return null;
+				}
+
+			}, _options);
+
+		} catch (IOException e) {
+			if (_eh != null) {
+				_eh.warning(e);
+			} else {
+				LOG.warning(e.getMessage());
+			}
+		} catch (JsonLdError e) {
+			if (_eh != null) {
+				_eh.warning(e);
+			} else {
+				LOG.warning(e.getMessage());
+			}
+		} finally {
+			cb.endDocument();
+		}
 	}
 
 	public static DocumentLoader getDocumentLoader() {
 		return _dl;
+	}
+
+	@Override
+	public void setErrorHandler(ErrorHandler eh) {
+		_eh = eh;
+	}
+
+	public static Node jsonld_java_Node__to__nxparser_Node(com.github.jsonldjava.core.RDFDataset.Node jsonldNode,
+			String baseURIstring) {
+		if (jsonldNode.isBlankNode()) {
+			return BNode.createBNode(baseURIstring.toString(), jsonldNode.getValue());
+		} else if (jsonldNode.isIRI()) {
+			return new Resource(jsonldNode.getValue());
+		} else if (jsonldNode.isLiteral()) {
+			return new Literal(jsonldNode.getValue(), jsonldNode.getLanguage(),
+					jsonldNode.getLanguage() == null ? new Resource(jsonldNode.getDatatype()) : null);
+		}
+
+		LOG.log(Level.WARNING, "Could not convert: {0}", new Object[] { jsonldNode });
+		return null;
 	}
 
 }
--- nxparser-parsers-external-jsonld-jsonld_java/src/test/java/org/semanticweb/yars/parsers/jsonld/jsonld_java/JsonLDParserTest.java
+++ nxparser-parsers-external-jsonld-jsonld_java/src/test/java/org/semanticweb/yars/parsers/jsonld/jsonld_java/JsonLDParserTest.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.assertEquals;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Collection;
 import java.util.HashSet;
 
@@ -12,6 +14,7 @@ import org.junit.runners.model.InitializationError;
 import org.semanticweb.yars.nx.BNode;
 import org.semanticweb.yars.nx.Node;
 import org.semanticweb.yars.nx.Nodes;
+import org.semanticweb.yars.nx.parser.Callback;
 import org.semanticweb.yars.nx.parser.NxParser;
 import org.semanticweb.yars.nx.parser.ParseException;
 import org.semanticweb.yars.parsers.external.json.jsonld_java.JsonLDparser;
@@ -25,7 +28,7 @@ import com.github.jsonldjava.core.JsonLdError;
 public class JsonLDParserTest {
 
 	@Test
-	public void test() throws JsonLdError, IOException, ParseException {
+	public void test() throws JsonLdError, IOException, ParseException, URISyntaxException {
 
 		// Default Person from http://json-ld.org/playground/index.html
 
@@ -42,18 +45,29 @@ public class JsonLDParserTest {
 		System.err.println();
 		System.err.println("Result:");
 
-		JsonLDparser jlp = new JsonLDparser();
-		jlp.parse(new ByteArrayInputStream(s.getBytes()), "http://example.org/");
+		JsonLDparser jlp = new JsonLDparser(new ByteArrayInputStream(s.getBytes()), new URI("http://example.org/"));
+		
+		final Collection<Nodes> actual = new HashSet<Nodes>();
+		jlp.parse(new Callback() {
+			@Override
+			protected void startDocumentInternal() {
+			}
 
-		Collection<Nodes> actual = new HashSet<Nodes>();
+			@Override
+			protected void endDocumentInternal() {
+			}
 
-		for (Node[] nx : jlp) {
-			System.err.println(Nodes.toString(nx));
+			@Override
+			protected void processStatementInternal(Node[] nx) {
+				System.err.println(Nodes.toString(nx));
 
-			// such that we don't need isomorphism check and can compare on the
-			// rdf term level.
-			actual.add(new Nodes(new Node[] { new BNode("b0"), nx[1], nx[2] }));
-		}
+				// such that we don't need isomorphism check and can compare on the
+				// rdf term level.
+				actual.add(new Nodes(new Node[] { new BNode("b0"), nx[1], nx[2] }));
+				
+			}		
+		});
+
 
 		String[] goldStandardStrings = new String[] {
 				// added xsd:string to the literals such that we can compare on
@@ -79,22 +93,30 @@ public class JsonLDParserTest {
 	}
 
 	@Test
-	public void test2() throws JsonLdError, IOException, ParseException {
+	public void test2() throws JsonLdError, IOException, ParseException, URISyntaxException {
 		// official test case
 		// http://json-ld.org/test-suite/reports/index.html#test_81b36000b509b1b4dd7fe3e4163b344d
 
 		String s = "{\n  \"@context\": {\n    \"t1\": \"http://example.com/t1\",\n    \"t2\": \"http://example.com/t2\",\n    \"term1\": \"http://example.com/term1\",\n    \"term2\": \"http://example.com/term2\",\n    \"term3\": \"http://example.com/term3\",\n    \"term4\": \"http://example.com/term4\",\n    \"term5\": \"http://example.com/term5\"\n  },\n  \"@id\": \"http://example.com/id1\",\n  \"@type\": \"t1\",\n  \"term1\": \"v1\",\n  \"term2\": {\"@value\": \"v2\", \"@type\": \"t2\"},\n  \"term3\": {\"@value\": \"v3\", \"@language\": \"en\"},\n  \"term4\": 4,\n  \"term5\": [50, 51]\n}";
 
-		JsonLDparser jlp = new JsonLDparser();
-		jlp.parse(new ByteArrayInputStream(s.getBytes()), "http://example.org/");
+		JsonLDparser jlp = new JsonLDparser(new ByteArrayInputStream(s.getBytes()), new URI("http://example.org/"));
+		final Collection<Nodes> actual = new HashSet<Nodes>();
+		jlp.parse(new Callback() {
+			@Override
+			protected void startDocumentInternal() {
+			}
 
-		Collection<Nodes> actual = new HashSet<Nodes>();
+			@Override
+			protected void endDocumentInternal() {
+			}
 
-		for (Node[] nx : jlp) {
-			Nodes ns = new Nodes(new Node[] { nx[0], nx[1], nx[2] });
-			actual.add(ns);
-			System.err.println(ns);
-		}
+			@Override
+			protected void processStatementInternal(Node[] nx) {
+				Nodes ns = new Nodes(new Node[] { nx[0], nx[1], nx[2] });
+				actual.add(ns);
+				System.err.println(ns);				
+			}		
+		});
 
 		Collection<Nodes> goldStandard = new HashSet<Nodes>();
 
@@ -121,7 +143,7 @@ public class JsonLDParserTest {
 	}
 
 	@Test
-	public void test3() throws JsonLdError, IOException, InitializationError {
+	public void test3() throws JsonLdError, IOException, InitializationError, URISyntaxException {
 		String s = "{  "
 				+ "\"@context\": "
 				+ "\"http://schema.org/\", "
@@ -135,13 +157,13 @@ public class JsonLDParserTest {
 		String[] goldStandardStrings = new String[] {
 				// added xsd:string to the literals such that we can compare on
 				// the rdf term level.
-				"<http://schema.org/id1> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://schema.org/t1> <http://schema.org/> .",
-				"<http://schema.org/id1> <http://schema.org/term1> \"v1\"^^<http://www.w3.org/2001/XMLSchema#string> <http://schema.org/> .",
-				"<http://schema.org/id1> <http://schema.org/term2> \"v2\"^^<http://schema.org/t2> <http://schema.org/> .",
-				"<http://schema.org/id1> <http://schema.org/term3> \"v3\"@en <http://schema.org/> .",
-				"<http://schema.org/id1> <http://schema.org/term4> \"4\"^^<http://www.w3.org/2001/XMLSchema#integer> <http://schema.org/> .",
-				"<http://schema.org/id1> <http://schema.org/term5> \"50\"^^<http://www.w3.org/2001/XMLSchema#integer> <http://schema.org/> .",
-				"<http://schema.org/id1> <http://schema.org/term5> \"51\"^^<http://www.w3.org/2001/XMLSchema#integer> <http://schema.org/> ." };
+				"<http://schema.org/id1> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://schema.org/t1> .",
+				"<http://schema.org/id1> <http://schema.org/term1> \"v1\"^^<http://www.w3.org/2001/XMLSchema#string> .",
+				"<http://schema.org/id1> <http://schema.org/term2> \"v2\"^^<http://schema.org/t2> .",
+				"<http://schema.org/id1> <http://schema.org/term3> \"v3\"@en .",
+				"<http://schema.org/id1> <http://schema.org/term4> \"4\"^^<http://www.w3.org/2001/XMLSchema#integer>  .",
+				"<http://schema.org/id1> <http://schema.org/term5> \"50\"^^<http://www.w3.org/2001/XMLSchema#integer>  .",
+				"<http://schema.org/id1> <http://schema.org/term5> \"51\"^^<http://www.w3.org/2001/XMLSchema#integer>  ." };
 
 		Collection<Nodes> goldStandard = new HashSet<Nodes>();
 		for (String line : goldStandardStrings) {
@@ -152,15 +174,25 @@ public class JsonLDParserTest {
 			}
 		}
 
-		JsonLDparser jlp = new JsonLDparser();
-		jlp.parse(new ByteArrayInputStream(s.getBytes()), "http://schema.org/");
+		JsonLDparser jlp = new JsonLDparser(new ByteArrayInputStream(s.getBytes()), new URI("http://schema.org/"));
 
-		Collection<Nodes> actual = new HashSet<Nodes>();
+		final Collection<Nodes> actual = new HashSet<Nodes>();
+		jlp.parse(new Callback() {
+			@Override
+			protected void startDocumentInternal() {
+			}
 
-		for (Node[] nx : jlp) {
-			actual.add(new Nodes(nx));
-		}
+			@Override
+			protected void endDocumentInternal() {
+			}
 
+			@Override
+			protected void processStatementInternal(Node[] nx) {
+				Nodes ns = new Nodes(new Node[] { nx[0], nx[1], nx[2] });
+				actual.add(ns);
+				System.err.println(ns);				
+			}		
+		});
 		assertEquals(goldStandard, actual);
 
 	}
