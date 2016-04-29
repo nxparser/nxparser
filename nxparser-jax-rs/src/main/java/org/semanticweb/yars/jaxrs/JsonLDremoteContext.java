5aa083000d1e8e8f2e61eea44a14b95460c89544
Tobias Käfer <tobias.kaefer@kit.edu>
2016-04-29 11:07:28 +0200

    documentation

--- nxparser-jax-rs/src/main/java/org/semanticweb/yars/jaxrs/JsonLDremoteContext.java
+++ nxparser-jax-rs/src/main/java/org/semanticweb/yars/jaxrs/JsonLDremoteContext.java
@@ -6,8 +6,18 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Can be used to specify remote contexts that will be used when serialising a
+ * RDF graph to JSON-LD in a HTTP response. The inclined coder is pointed to the
+ * <a href=
+ * "http://github.com/jsonld-java/jsonld-java#loading-contexts-from-classpathjar">
+ * remote context caching capabilities of jsonld_java</a>.
+ * 
+ * @author Tobias Käfer
+ *
+ */
 @Documented
-@Target({ElementType.METHOD, ElementType.TYPE})
+@Target({ ElementType.METHOD, ElementType.TYPE })
 @Retention(RetentionPolicy.RUNTIME)
 public @interface JsonLDremoteContext {
 	String[] value();
