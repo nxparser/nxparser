b1c79dda7f3798b1a2284329a1a669cc7655eb7f
Tobias Käfer <tobias.kaefer@kit.edu>
2016-05-18 00:11:36 +0200

    Annotation to redirect non-slash to slash URIs.

---
+++ nxparser-jax-rs/src/main/java/org/semanticweb/yars/jaxrs/RedirectMissingTrailingSlash.java
@@ -0,0 +1,17 @@
+package org.semanticweb.yars.jaxrs;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import javax.ws.rs.NameBinding;
+
+/**
+ * Annotation to cause the following behaviour:
+ * The resource without a slash does not exist, but a resource with a slash
+ * would. Assuming the requester meant the latter, we fail gently by answering
+ * with a redirect to the resource with slash.
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@NameBinding
+public @interface RedirectMissingTrailingSlash {
+}
---
+++ nxparser-jax-rs/src/main/java/org/semanticweb/yars/jaxrs/RedirectMissingTrailingSlashFilter.java
@@ -0,0 +1,35 @@
+package org.semanticweb.yars.jaxrs;
+
+import java.io.IOException;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.ext.Provider;
+
+/**
+ * Filter to redirect from resources whose URI does not end in a slash to
+ * resources with a slash using a <tt>301 Moved Permanently</tt> HTTP response.
+ * 
+ * Requires the resources which should exhibit this behaviour to be annotated
+ * using {@link RedirectMissingTrailingSlash}.
+ * 
+ * @author Tobias Käfer
+ *
+ */
+@Provider
+@RedirectMissingTrailingSlash
+public class RedirectMissingTrailingSlashFilter implements ContainerRequestFilter {
+
+	@Override
+	public void filter(ContainerRequestContext requestContext) throws IOException {
+		if (!requestContext.getUriInfo().getPath().endsWith("/")) {
+			requestContext.abortWith(Response.status(Status.MOVED_PERMANENTLY)
+					.location(requestContext.getUriInfo().getAbsolutePathBuilder().path("/").build()).build());
+
+		}
+
+	}
+
+}
