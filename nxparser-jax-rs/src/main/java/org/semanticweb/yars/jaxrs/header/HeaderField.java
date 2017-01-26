1b8021faafbee5c5c9c21ef89f7633aade96b3d6
Tobias Käfer <tobias.kaefer@kit.edu>
2017-01-26 17:43:41 +0100

    java <=8 compatibility not just in code but also in the annotation

--- nxparser-jax-rs/src/main/java/org/semanticweb/yars/jaxrs/header/HeaderField.java
+++ nxparser-jax-rs/src/main/java/org/semanticweb/yars/jaxrs/header/HeaderField.java
@@ -1,11 +1,10 @@
 package org.semanticweb.yars.jaxrs.header;
 
-import java.lang.annotation.Repeatable;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
 @Retention(RetentionPolicy.RUNTIME)
-@Repeatable(value = InjectHeaders.class)
+// requires Java 8: @Repeatable(value = InjectHeaders.class)
 public @interface HeaderField {
 	String name();
 	String value();
