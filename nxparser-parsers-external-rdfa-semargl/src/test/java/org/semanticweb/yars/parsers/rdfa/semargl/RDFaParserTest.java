18ff1078a4b4d7dd7a63a4efcc5764264592754a
Tobias Käfer <tobias.kaefer@kit.edu>
2017-01-31 15:47:32 +0100

    adding a test to see the output of plain semargl w/o tagsoup

--- nxparser-parsers-external-rdfa-semargl/src/test/java/org/semanticweb/yars/parsers/rdfa/semargl/RDFaParserTest.java
+++ nxparser-parsers-external-rdfa-semargl/src/test/java/org/semanticweb/yars/parsers/rdfa/semargl/RDFaParserTest.java
@@ -7,23 +7,32 @@ import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.net.URLConnection;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import org.junit.Test;
 import org.semanticweb.yars.nx.Node;
 import org.semanticweb.yars.nx.Nodes;
 import org.semanticweb.yars.nx.parser.Callback;
+import org.semarglproject.rdf.NTriplesSerializer;
 import org.semarglproject.rdf.ParseException;
+import org.semarglproject.rdf.rdfa.RdfaParser;
+import org.semarglproject.sink.CharOutputSink;
+import org.semarglproject.source.StreamProcessor;
 import org.xml.sax.SAXException;
 
 public class RDFaParserTest {
 
 	@Test
 	public void test() throws SAXException, ParseException, IOException, URISyntaxException {
-		String uri = "http://schema.org/Person";
+		String uri = "https://creativecommons.org/ns" ; //"http://schema.org/Person";
 		URL u = new URL(uri);
 
-		RDFaParser rp = new RDFaParser(u.openStream(), u.toURI());
+		URLConnection connection = u.openConnection();
+		connection.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
+		connection.connect();
+		
+		RDFaParser rp = new RDFaParser(connection.getInputStream(), u.toURI());
 		
 		OutputStream os = System.out;
 		os = new DevNull(); // comment out for output of the quads.
@@ -58,6 +67,25 @@ public class RDFaParserTest {
 		System.err.println("Quads processed: " + count);
 		if (count.get() == 0)
 			fail();
+	}
+	
+	public void plainSemarglTest() throws ParseException, IOException {
+
+		CharOutputSink cos = new CharOutputSink();
+		cos.connect(System.out);
+
+		StreamProcessor sp = new StreamProcessor(RdfaParser.connect(NTriplesSerializer.connect(cos)));
+
+		// "http://schema.org/Person" would kill it
+		String uri = "https://creativecommons.org/ns"; 
+		URL u = new URL(uri);
+
+		URLConnection connection = u.openConnection();
+		connection.setRequestProperty("User-Agent",
+				"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
+		connection.connect();
+		sp.process(connection.getInputStream(), uri);
+
 	}
 
 	public static class DevNull extends OutputStream {
