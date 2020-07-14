a7eecdc3cd226dfc9a00e7f029d964e5d6a04bd5
Tobias Käfer <6708974+kaefer3000@users.noreply.github.com>
2020-07-14 15:56:29 +0200

    removed the need for network lookup

--- nxparser-parsers-external-rdfa-semargl/src/test/java/org/semanticweb/yars/parsers/rdfa/semargl/RDFaParserTest.java
+++ nxparser-parsers-external-rdfa-semargl/src/test/java/org/semanticweb/yars/parsers/rdfa/semargl/RDFaParserTest.java
@@ -2,6 +2,7 @@ package org.semanticweb.yars.parsers.rdfa.semargl;
 
 import static org.junit.Assert.*;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.PrintWriter;
@@ -17,12 +18,19 @@ public class RDFaParserTest {
 
 	@Test
 	public void test() throws SAXException, ParseException, IOException {
-		String uri = "http://schema.org/Person";
-		URL u = new URL(uri);
 
 		RDFaParser rp = new RDFaParser();
 
-		rp.parse(u.openStream(), uri);
+		rp.parse(new ByteArrayInputStream(("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" + 
+				"<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML+RDFa 1.0//EN\"\n" + 
+				"    \"http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd\">\n" + 
+				"<html xmlns=\"http://www.w3.org/1999/xhtml\"\n" + 
+				"    xmlns:foaf=\"http://xmlns.com/foaf/0.1/\"\n" + 
+				"    version=\"XHTML+RDFa 1.0\" xml:lang=\"en\">\n" + 
+				"  <body about=\"http://example.org/#me\">\n" + 
+				"    <p>Hi, I am <span property=\"foaf:name\">John</span>.</p>\n" + 
+				"  </body>\n" + 
+				"</html>").getBytes("utf-8")), "http://example.org/");
 
 		int count = 0;
 
