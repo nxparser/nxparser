851b9db3a4e40a7fd5b3b14a55eca2e14bc4d84b
Tobias Käfer <6708974+kaefer3000@users.noreply.github.com>
2020-11-27 18:02:38 +0100

    java 8

--- nxparser-jax-rs/src/main/java/org/semanticweb/yars/jaxrs/header/HeaderField.java
+++ nxparser-jax-rs/src/main/java/org/semanticweb/yars/jaxrs/header/HeaderField.java
@@ -1,10 +1,11 @@
 package org.semanticweb.yars.jaxrs.header;
 
+import java.lang.annotation.Repeatable;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
 @Retention(RetentionPolicy.RUNTIME)
-// requires Java 8: @Repeatable(value = InjectHeaders.class)
+@Repeatable(value = InjectHeaders.class)
 public @interface HeaderField {
 	String name();
 	String value();
